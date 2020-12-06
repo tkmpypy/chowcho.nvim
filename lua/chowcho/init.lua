8eb3ab199d02b0eafcd77c816d8b52b416842428
tkmpypy <tkmpypy@gmail.com>
2020-12-06 23:42:17 +0900

    Update

--- lua/chowcho/init.lua
+++ lua/chowcho/init.lua
@@ -2,39 +2,76 @@ local chowcho = {}
 
 local _float_wins = {}
 local _wins = {}
-local _exclude_ft = vim.g.chowcho_exclude_filetypes
+
+-- for default options
+local _opt = {
+  text_color = '#FFFFFF',
+  bg_color = '#555555',
+  active_border_color = '#D2D',
+  exclude_filetypes = {'LuaTree', 'packer'},
+  border_style = 'default'
+}
+
+local _border_style = {
+  default = {
+    topleft = '╔',
+    topright = '╗',
+    top = '═',
+    left = '║',
+    right = '║',
+    botleft = '╚',
+    botright = '╝',
+    bot = '═'
+  },
+  fancy = {} -- TODO(tkmpypy): unimplemented
+}
 
 local str = function(v) return v .. '' end
 
-local centering_str = function(src, target) 
-  local ws = math.ceil(#target / 2)
-  for _=2, ws do
-    src = ' ' .. src
+local create_content = function(label)
+  local no = label[1]
+  local fname = label[2]
+  local content = '[' .. no .. ']' .. ' ' .. fname
+  local width = #content + 2
+  local border = _border_style[_opt.border_style]
+
+  -- top and bottom
+  local top = ''
+  local bottom = ''
+  for i = 1, width do
+    if (i == 1) then
+      top = border.topleft .. top
+      bottom = border.botleft .. bottom
+      content = border.left .. content
+    elseif (i == width) then
+      top = top .. border.topright
+      bottom = bottom .. border.botright
+      content = content .. border.right
+    else
+      top = top .. border.top
+      bottom = bottom .. border.bot
+    end
   end
 
-  return src
+  return {top, content, bottom}
 end
 
 local create_floating_win = function(x, y, win, label)
   local buf = vim.api.nvim_create_buf(false, true)
 
-  local w = 0
   local win_num = label[1]
-  if (#label[1] >= #label[2]) then
-    w = #label[2]
-    label[2] = centering_str(label[2], label[1])
-  else
-    w = #label[2]
-    label[1] = centering_str(label[1], label[2])
-  end
-  vim.api.nvim_buf_set_lines(buf, 0, -1, true, label)
+  local content_tbl = create_content(label)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, true, content_tbl)
 
+  local _x = x - (math.ceil(#content_tbl[2] / 2)) + 2
+
+  local _content_w = vim.api.nvim_strwidth(content_tbl[2])
   local opt = {
     win = win,
     relative = 'win',
-    width = w,
-    height = 2,
-    col = x,
+    width = _content_w,
+    height = 3,
+    col = _x,
     row = y,
     anchor = 'NW',
     style = 'minimal',
@@ -42,6 +79,7 @@ local create_floating_win = function(x, y, win, label)
   }
 
   local window = vim.api.nvim_open_win(buf, false, opt)
+
   vim.api.nvim_win_set_option(window, 'winhl', 'Normal:ChowchoFloat')
   table.insert(_float_wins, window)
   table.insert(_wins, {no = win_num, win = win})
@@ -58,14 +96,15 @@ end
 chowcho.run = function()
   _wins = {}
   local wins = vim.api.nvim_list_wins()
+
+  vim.cmd('hi! ChowchoFloat guifg=' .. _opt.text_color .. ' guibg=' ..
+              _opt.bg_color)
   for i, v in ipairs(wins) do
     local pos = calc_center_win_pos(v)
     local buf = vim.api.nvim_win_get_buf(v)
-    local fname = vim.fn.expand('#'..buf..':t')
-    create_floating_win(pos.w, pos.h, v, {
-      str(i), 
-      fname
-    })
+    local fname = vim.fn.expand('#' .. buf .. ':t')
+    if (fname == '') then fname = 'NO NAME' end
+    create_floating_win(pos.w, pos.h, v, {str(i), fname})
   end
 
   local timer = vim.loop.new_timer()
@@ -94,6 +133,31 @@ chowcho.on_cursor_moved = function()
       vim.api.nvim_win_close(v, true)
       _float_wins[i] = nil
     end
+  end
+end
+
+--[[
+{
+  text_color = '#FFFFFF'
+  bg_color = '#555555'
+  active_border_color = '#D2D'
+  exclude_filetypes = {'LuaTree', 'packer'},
+  border_style = 'fancy' -- 'default', 'fancy'
+}
+--]]
+chowcho.setup = function(opt)
+  if (type(opt) == 'table') then
+    if (opt.text_color) then _opt.text_color = opt.text_color end
+    if (opt.bg_color) then _opt.bg_color = opt.bg_color end
+    if (opt.active_border_color) then
+      _opt.active_border_color = opt.active_border_color
+    end
+    if (opt.exclude_filetypes) then
+      _opt.exclude_filetypes = opt.exclude_filetypes
+    end
+    if (opt.border_style) then _opt.border_style = opt.border_style end
+  else
+    error('[chowcho.nvim] option is must be table')
   end
 end
 
--- plugin/chowcho.vim
+++ plugin/chowcho.vim
@@ -4,11 +4,6 @@ endif
 
 let g:loaded_chowcho = 1
 
-hi! ChowchoFloat guifg=#FF0000 guibg=NONE guisp=NONE gui=NONE cterm=bold
-
-" TODO(tkmpypy)
-let g:chowcho_exclude_filetypes = []
-
 autocmd CursorMoved,CursorMovedI * call luaeval('require("chowcho").on_cursor_moved()')
 
 command! -nargs=0 -bang Chowcho call luaeval('require("chowcho").run()')
