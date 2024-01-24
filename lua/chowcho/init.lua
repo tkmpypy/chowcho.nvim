b22f2545a939160b9af1a180f805b8e66b0a615b
tkmpypy <tkmpypy@gmail.com>
2024-01-24 10:52:47 +0900

    style: fmt

--- lua/chowcho/init.lua
+++ lua/chowcho/init.lua
@@ -1,4 +1,4 @@
-local ui = require('chowcho.ui')
+local ui = require("chowcho.ui")
 
 local chowcho = {}
 
@@ -7,10 +7,10 @@ local _float_wins = {}
 -- for default options
 local _opt = {
   icon_enabled = false,
-  text_color = '#FFFFFF',
+  text_color = "#FFFFFF",
   bg_color = nil,
-  active_border_color = '#B400C8',
-  border_style = 'default',
+  active_border_color = "#B400C8",
+  border_style = "default",
   use_exclude_default = true,
   exclude = nil,
   zindex = 10000,
@@ -18,35 +18,39 @@ local _opt = {
 
 local _border_style = {
   default = {
-    topleft = '╔',
-    topright = '╗',
-    top = '═',
-    left = '║',
-    right = '║',
-    botleft = '╚',
-    botright = '╝',
-    bot = '═'
+    topleft = "╔",
+    topright = "╗",
+    top = "═",
+    left = "║",
+    right = "║",
+    botleft = "╚",
+    botright = "╝",
+    bot = "═",
   },
   rounded = {
-    topleft = '╭',
-    topright = '╮',
-    top = '─',
-    left = '│',
-    right = '│',
-    botleft = '╰',
-    botright = '╯',
-    bot = '─'
-  }
+    topleft = "╭",
+    topright = "╮",
+    top = "─",
+    left = "│",
+    right = "│",
+    botleft = "╰",
+    botright = "╯",
+    bot = "─",
+  },
 }
 
-local str = function(v) return v .. '' end
+local str = function(v)
+  return v .. ""
+end
 
 local is_enable_icon = function(opt)
   if opt.icon_enabled then
-    local loaded_devicons = vim.api.nvim_get_var('loaded_devicons')
-    if loaded_devicons < 1 then return false end
+    local loaded_devicons = vim.api.nvim_get_var("loaded_devicons")
+    if loaded_devicons < 1 then
+      return false
+    end
 
-    return require('nvim-web-devicons').has_loaded()
+    return require("nvim-web-devicons").has_loaded()
   end
 
   return false
@@ -56,30 +60,28 @@ local calc_center_win_pos = function(win)
   local w = vim.api.nvim_win_get_width(win)
   local h = vim.api.nvim_win_get_height(win)
 
-  return {w = math.ceil(w / 2), h = math.ceil(h / 2)}
+  return { w = math.ceil(w / 2), h = math.ceil(h / 2) }
 end
 
 local hi_active_float = function(f_win, opt)
   for _, v in pairs(_border_style[opt.border_style]) do
-    vim.fn.matchadd("ChowchoActiveFloat", v, 0, -1, {window = f_win})
+    vim.fn.matchadd("ChowchoActiveFloat", v, 0, -1, { window = f_win })
   end
 end
 
 local set_highlight = function(opt)
-  if (opt.bg_color == nil or opt.bg_color == '') then
-    vim.cmd('hi! ChowchoFloat guifg=' .. opt.text_color)
-    vim.cmd('hi! ChowchoActiveFloat guifg=' .. opt.active_border_color)
+  if opt.bg_color == nil or opt.bg_color == "" then
+    vim.cmd("hi! ChowchoFloat guifg=" .. opt.text_color)
+    vim.cmd("hi! ChowchoActiveFloat guifg=" .. opt.active_border_color)
   else
-    vim.cmd('hi! ChowchoFloat guifg=' .. opt.text_color .. ' guibg=' ..
-                opt.bg_color)
-    vim.cmd('hi! ChowchoActiveFloat guifg=' .. opt.active_border_color ..
-                ' guibg=' .. opt.bg_color)
+    vim.cmd("hi! ChowchoFloat guifg=" .. opt.text_color .. " guibg=" .. opt.bg_color)
+    vim.cmd("hi! ChowchoActiveFloat guifg=" .. opt.active_border_color .. " guibg=" .. opt.bg_color)
   end
 end
 
 local win_close = function()
   for i, v in ipairs(_float_wins) do
-    if (v ~= nil) then
+    if v ~= nil then
       vim.api.nvim_win_close(v, true)
       _float_wins[i] = nil
     end
@@ -99,29 +101,34 @@ chowcho.run = function(fn, opt)
 
   local _wins = {}
   for i, v in ipairs(wins) do
-    if not vim.api.nvim_win_is_valid(v) then goto continue end
+    if not vim.api.nvim_win_is_valid(v) then
+      goto continue
+    end
     local pos = calc_center_win_pos(v)
     local buf = vim.api.nvim_win_get_buf(v)
-    local bt = vim.api.nvim_buf_get_option(buf, 'buftype')
-    if bt ~= 'prompt' then
-      local fname = vim.fn.expand('#' .. buf .. ':t')
+    local bt = vim.api.nvim_buf_get_option(buf, "buftype")
+    if bt ~= "prompt" then
+      local fname = vim.fn.expand("#" .. buf .. ":t")
 
       if opt_local.use_exclude_default then
-        if fname == '' then goto continue end
+        if fname == "" then
+          goto continue
+        end
       end
 
       if opt_local.exclude ~= nil then
-        if opt_local.exclude(buf, v) then goto continue end
+        if opt_local.exclude(buf, v) then
+          goto continue
+        end
       end
 
-      local icon, hl_name = '', ''
+      local icon, hl_name = "", ""
       if is_enable_icon(opt_local) then
         icon, hl_name = ui.get_icon(fname)
-        fname = icon .. ' ' .. fname
+        fname = icon .. " " .. fname
       end
-      local bufnr, f_win, win = ui.create_floating_win(pos.w, pos.h, v,
-                                                       {str(i), fname},
-                                                       _border_style[opt_local.border_style],_opt.zindex)
+      local bufnr, f_win, win =
+        ui.create_floating_win(pos.w, pos.h, v, { str(i), fname }, _border_style[opt_local.border_style], _opt.zindex)
 
       if is_enable_icon(opt_local) then
         local line = vim.api.nvim_buf_get_lines(bufnr, 1, 2, false)
@@ -132,31 +139,36 @@ chowcho.run = function(fn, opt)
       table.insert(_float_wins, f_win)
       table.insert(_wins, win)
 
-      if (v == current_win) then hi_active_float(f_win, opt_local) end
+      if v == current_win then
+        hi_active_float(f_win, opt_local)
+      end
     end
     ::continue::
   end
 
   local timer = vim.loop.new_timer()
-  timer:start(10, 0, vim.schedule_wrap(function()
-    local success, val = pcall(vim.fn.getchar)
-    if success then
-      val = vim.fn.nr2char(val)
-      if (val ~= nil) then
-        for _, v in ipairs(_wins) do
-          if (v ~= nil) then
-            if (v.no == str(val)) then
-              (fn or vim.api.nvim_set_current_win)(v.win)
-              break
+  timer:start(
+    10,
+    0,
+    vim.schedule_wrap(function()
+      local success, val = pcall(vim.fn.getchar)
+      if success then
+        val = vim.fn.nr2char(val)
+        if val ~= nil then
+          for _, v in ipairs(_wins) do
+            if v ~= nil then
+              if v.no == str(val) then
+                (fn or vim.api.nvim_set_current_win)(v.win)
+                break
+              end
             end
           end
         end
       end
-    end
-    win_close()
-    timer:stop()
-  end))
-
+      win_close()
+      timer:stop()
+    end)
+  )
 end
 
 --[[
@@ -171,19 +183,33 @@ end
 }
 --]]
 chowcho.setup = function(opt)
-  if (type(opt) == 'table') then
-    if opt.icon_enabled ~= nil then _opt.icon_enabled = opt.icon_enabled end
-    if opt.text_color ~= nil then _opt.text_color = opt.text_color end
-    if opt.bg_color ~= nil then _opt.bg_color = opt.bg_color end
+  if type(opt) == "table" then
+    if opt.icon_enabled ~= nil then
+      _opt.icon_enabled = opt.icon_enabled
+    end
+    if opt.text_color ~= nil then
+      _opt.text_color = opt.text_color
+    end
+    if opt.bg_color ~= nil then
+      _opt.bg_color = opt.bg_color
+    end
     if opt.active_border_color ~= nil then
       _opt.active_border_color = opt.active_border_color
     end
-    if opt.border_style ~= nil then _opt.border_style = opt.border_style end
-    if opt.use_exclude_default ~= nil then _opt.use_exclude_default = opt.use_exclude_default end
-    if opt.exclude ~= nil then _opt.exclude = opt.exclude end
-    if opt.zindex ~= nil then _opt.zindex = opt.zindex end
+    if opt.border_style ~= nil then
+      _opt.border_style = opt.border_style
+    end
+    if opt.use_exclude_default ~= nil then
+      _opt.use_exclude_default = opt.use_exclude_default
+    end
+    if opt.exclude ~= nil then
+      _opt.exclude = opt.exclude
+    end
+    if opt.zindex ~= nil then
+      _opt.zindex = opt.zindex
+    end
   else
-    error('[chowcho.nvim] option is must be table')
+    error("[chowcho.nvim] option is must be table")
   end
 end
 
